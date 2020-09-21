3c87511fab6e80cdc3759655481adaf1f7eca357
Masato Bito <m@btmst.jp>
2020-09-21 13:25:19 +0900

    add TodoComponent

---
+++ react-redux-ts/src/TodoComponent.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+type Props = {
+    todos: string[];
+    onClickAddButton: (todo: string) => void;
+};
+
+type State = {
+    text: string;
+};
+
+export default class extends React.Component<Props, State> {
+    state = {
+        text: "",
+    };
+
+    public render() {
+        const { todos } = this.props;
+        const { text } = this.state;
+        return (
+            <div style={{ width: "500px", margin: "0 auth"}}>
+                <h1>Todo List</h1>
+                <input type="text" value={text} onChange={this.onTextChange} />
+                <button onClick={this.onClickAddButton}>Add Todo</button>
+                <ul>
+                    {todos.map((todo, i) => (
+                        <li key={i}>{todo}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    private onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ text: e.currentTarget.value });
+    };
+
+    private onClickAddButton = () => {
+        const { onClickAddButton } = this.props;
+        const { text } = this.state;
+        onClickAddButton(text);
+    }
+}
